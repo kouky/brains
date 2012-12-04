e4ab330b285f8b97f1628f6461bc9c86fd4d3b9d
Michael Koukoullis <mkoukoullis@gmail.com>
2012-12-04 22:59:17 +1100

    world:add_player/2 function - returns a world state with a player added to it

--- world.erl
+++ world.erl
@@ -1,4 +1,8 @@
 -module(world).
--export([create/0]).
+-export([create/0, add_player/2]).
 
 create() -> { world, {players, []}, {mobs, []} }.
+
+add_player(World, Player) ->
+  { world, {players, P}, {mobs, M} } = World,
+  { world, {players, [Player|P]}, {mobs, M} }.
